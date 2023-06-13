7af28dc628ddda3e1c700406309a430a567fc742
nabil-waris <nabilwaris@protonmail.com>
2023-06-13 19:44:38 +0500

    First Commit

---
+++ index.js
@@ -0,0 +1,61 @@
+"use strict";
+const apiKey = "61c08fda24558fac2f955e6d98018886";
+const weatherDataEle = document.getElementById("weather-data");
+const cityInputEle = document.getElementById("city-input");
+const formEle = document.querySelector("form");
+formEle?.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const cityValue = cityInputEle?.value || ""; // Use optional chaining and provide a default value
+    getWeatherData(cityValue);
+});
+async function getWeatherData(cityValue) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`);
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        const temperature = Math.round(data.main.temp);
+        const description = data.weather[0].description;
+        const icon = data.weather[0].icon;
+        const details = [
+            `Feels like: ${Math.round(data.main.feels_like)}`,
+            `Humidity: ${data.main.humidity}%`,
+            `Wind speed: ${data.wind.speed} m/s`,
+        ];
+        const iconElement = weatherDataEle?.querySelector(".icon");
+        if (iconElement) {
+            iconElement.innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="weather icon">`;
+        }
+        const tempElement = weatherDataEle?.querySelector(".temperature");
+        if (tempElement) {
+            tempElement.textContent = `${temperature}°C`;
+        }
+        const DescripElement = weatherDataEle?.querySelector(".description");
+        if (DescripElement) {
+            DescripElement.textContent = description;
+        }
+        const detailElement = weatherDataEle?.querySelector(".details");
+        if (detailElement) {
+            detailElement.innerHTML = details.map((dat) => `<div>${dat}</div>`).join('');
+        }
+    }
+    catch (error) {
+        const iconElement = weatherDataEle?.querySelector(".icon");
+        if (iconElement) {
+            iconElement.innerHTML = "";
+        }
+        const tempElement = weatherDataEle?.querySelector(".temperature");
+        if (tempElement) {
+            tempElement.textContent = "";
+        }
+        const DescripElement = weatherDataEle?.querySelector(".description");
+        if (DescripElement) {
+            DescripElement.textContent = "An error occured, please try again later";
+        }
+        const detailElement = weatherDataEle?.querySelector(".details");
+        if (detailElement) {
+            detailElement.innerHTML = "";
+        }
+    }
+}
